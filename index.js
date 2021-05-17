912880cd7126e671376f1c255521c5d56b8c93e9
Grant Cloyd <dgcloyd@gmail.com>
2021-05-17 14:57:48 -0500

    commit tree fc49c5d06477219087191f4baebd3a57f5f6f32f

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+const distanceFromHqInBlocks = (num) => {
+    const distance =  num > 42 ? num - 42 : 42 - num;
+    return distance; 
+}
+
+const distanceFromHqInFeet = (num) => distanceFromHqInBlocks(num) * 264;
+
+const distanceTravelledInFeet = (num1, num2) => {
+   const distance = num1 > num2 ? num1 - num2 : num2-num1;  
+   return distance * 264;
+}
+
+const calculatesFarePrice = (start, end) =>{
+  let cost = distanceTravelledInFeet(start, end);
+  if (cost < 400){
+      return 0;
+  }
+  else if (cost >= 400 && cost <= 2000){
+     cost = (cost - 400) * 0.02;
+     return cost;
+  } else if (cost > 2000 && cost <= 2500 ){
+      cost = 25;
+      return cost;
+  } else {
+    return `cannot travel that far`
+  }
+} 
